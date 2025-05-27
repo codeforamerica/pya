a98aa1be1731671380b3fed862efc451d648a94a
Anisha Ramnani <anisha.ramnani@gmail.com>
2025-05-27 15:28:28 -0400

    create first page and landing page of fyst

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+window.jQuery = $;
+window.$ = $;
+
+RailsUJS.start();
+ActiveStorage.start();
+
+import "../lib/honeycrisp";
+"./app/javascript/lib/honeycrisp.js":
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root "pages#home"
+  get "first_page" => "pages#first_page"
 end
