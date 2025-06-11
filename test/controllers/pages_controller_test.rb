1f48d6c26d116496f2863cef8ec25c317a31ce10
Anisha Ramnani <anisha.ramnani@gmail.com>
2025-06-11 16:28:34 -0400

    Revert "create test failure"
    This reverts commit 6b5986718d9a5a126b74a9ea70e3ba7f85b64b33.

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get first_page_url
-    assert_response 400
+    assert_response :success
   end
 end
