15e0d97d3b52224fc0ac5dbd0e297b0242a23bc5
Arin Choi <achoi@codeforamerica.org>
2025-06-13 15:33:55 -0700

    Fix test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    # get first_page_url
+    get first_page_url
     assert_response :success
   end
 end
