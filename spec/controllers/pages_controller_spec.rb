9e7b55d310988699e2064f583a2e83f409d8d3b7
Anisha Ramnani <aramnani@codeforamerica.org>
2025-06-17 17:29:06 -0400

    change test to be a bit more interesting:
    Co-authored-by: Tahsina Islam <tislam@codeforamerica.org>

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe PagesController, type: :request  do
   describe "/" do
     it "returns a successful response" do
-      get "/"
+      get first_page_url
       expect(response).to be_ok
     end
   end
