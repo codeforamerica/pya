17f469b5e883b82809b068869e40c2d05757002c
Arin Choi <achoi@codeforamerica.org>
2025-06-26 15:32:57 -0700

    Create table + model for state_file_archived_intakes and state_file_archived_intake_access_logs

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_26_214538) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
+  create_table "state_file_archived_intake_access_logs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.jsonb "details", default: "{}"
+    t.integer "event_type"
+    t.bigint "state_file_archived_intakes_id"
+    t.index ["state_file_archived_intakes_id"], name: "idx_on_state_file_archived_intakes_id_e878049c06"
+  end
+
+  create_table "state_file_archived_intakes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email_address"
+    t.string "phone_number"
+    t.string "fake_address_1"
+    t.string "fake_address_2"
+    t.string "hashed_ssn"
+    t.string "mailing_apartment"
+    t.string "mailing_city"
+    t.string "mailing_state"
+    t.string "mailing_street"
+    t.string "mailing_zip"
+    t.string "state_code"
+    t.integer "tax_year"
+    t.boolean "unsubscribed_from_email", default: false, null: false
+  end
+
+  add_foreign_key "state_file_archived_intake_access_logs", "state_file_archived_intakes", column: "state_file_archived_intakes_id"
 end
