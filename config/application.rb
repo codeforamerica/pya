12f16265da7cde8dc1079febe53ff89df7f5651c
Drew Proebstel <dproebstel@codeforamerica.org>
2025-07-02 16:16:24 -0700

    git ignore

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,6 @@ module Pya
 
     config.i18n.default_locale = :en
     config.i18n.available_locales = [ :en, :es ]
-
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
