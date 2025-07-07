92cbc74a6604673b3bbd39b9c1791e274fd3a152
Drew Proebstel <dproebstel@codeforamerica.org>
2025-07-07 12:12:06 -0700

    wip dropdown menu'

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import ClientMenuComponent from "ClientMenuComponent"
+import "rails-ujs"
 
 window.jQuery = $;
 window.$ = $;
 
 RailsUJS.start();
 ActiveStorage.start();
+
+document.addEventListener("turbo:load", () => {
+    ClientMenuComponent();
+});
