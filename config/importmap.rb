5c3ba868a92454bd5fb55e7ece4b73dd673870ea
Arin Choi <achoi@codeforamerica.org>
2025-07-11 09:57:48 -0700

    Pin necessary modules and include "application" since thats the entrypoint - without this importmap will load without executing application.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+pin "@rails/activestorage", to: "https://cdn.jsdelivr.net/npm/@rails/activestorage@7.0.0/lib/assets/compiled/activestorage.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "jquery", to: "https://code.jquery.com/jquery-3.7.1.min.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "ClientMenuComponent", to: "ClientMenuComponent.js"
-pin "rails-ujs", to: "https://cdn.skypack.dev/@rails/ujs"
