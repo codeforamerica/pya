b9ebac291506f892886e0619098f3eade3f4266b
Arin Choi <achoi@codeforamerica.org>
2025-07-11 10:03:27 -0700

    Use the already available activestorage

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@rails/activestorage", to: "https://cdn.jsdelivr.net/npm/@rails/activestorage@7.0.0/lib/assets/compiled/activestorage.js"
+pin "@rails/activestorage", to: "activestorage.esm.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
