2bdcc175af3dc574165bfe69ff33b2d094322593
Drew Proebstel <dproebstel@codeforamerica.org>
2025-07-14 11:02:39 -0700

    rubocop

--- app/forms/year_select_form.rb
+++ app/forms/year_select_form.rb
@@ -2,5 +2,5 @@ class YearSelectForm < Form
   attr_accessor :year
 
   validates :year, presence: true
-  validates :year, inclusion: { in: %w[2023 2024]}, if: -> { year.present? }
+  validates :year, inclusion: { in: %w[2023 2024] }, if: -> { year.present? }
 end
