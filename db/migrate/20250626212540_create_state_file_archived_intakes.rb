c5545d047fb066b5d52e60f44981f7f91554b945
Drew Proebstel <dproebstel@codeforamerica.org>
2025-07-17 16:36:17 -0700

    update contact preference case

--- db/migrate/20250626212540_create_state_file_archived_intakes.rb
+++ db/migrate/20250626212540_create_state_file_archived_intakes.rb
@@ -14,7 +14,7 @@ class CreateStateFileArchivedIntakes < ActiveRecord::Migration[8.0]
       t.string :mailing_zip
       t.string :state_code
       t.integer :tax_year
-      t.string :contact_preference
+      t.integer :contact_preference, default: 0, null: false
       t.boolean :unsubscribed_from_email, default: false, null: false
     end
   end
--- db/migrate/20250717232509_change_contact_preference_to_enum.rb
+++
@@ -1,6 +0,0 @@
-class ChangeContactPreferenceToEnum < ActiveRecord::Migration[8.0]
-  def change
-    remove_column :state_file_archived_intakes, :contact_preference, :string
-    add_column :state_file_archived_intakes, :contact_preference, :integer, default: 0, null: false
-  end
-end
