f1a01c7e0845d03426d77b201688564197f83038
Drew Proebstel <dproebstel@codeforamerica.org>
2025-07-31 10:58:30 -0700

    add spec

--- app/controllers/mailing_address_validation_controller.rb
+++ app/controllers/mailing_address_validation_controller.rb
@@ -1,25 +1,23 @@
 require 'csv'
 
 class MailingAddressValidationController < BaseController
-  # before_action :is_intake_locked
-  # before_action :confirm_code_and_ssn_verification
+  before_action :is_intake_locked
+  before_action :confirm_code_and_ssn_verification
   def edit
     @addresses = current_archived_intake.address_challenge_set
     @form = MailingAddressValidationForm.new(addresses: @addresses, current_address: current_archived_intake.full_address)
   end
 
   def update
-    @form = MailingAddressValidationForm.new(mailing_address_validation_form_params, addresses: @addresses, current_address: current_archived_intake.full_address)
     @addresses = current_archived_intake.address_challenge_set
-
-
+    @form = MailingAddressValidationForm.new(mailing_address_validation_form_params, addresses: @addresses, current_address: current_archived_intake.full_address)
     if @form.valid?
       session[:mailing_verified] = true
 
-      redirect_to state_file_archived_intakes_pdfs_path
-    elsif params["state_file_archived_intakes_mailing_address_validation_form"].present?
+      redirect_to root_path
+    elsif params["mailing_address_validation_form"].present?
       current_archived_intake.update(permanently_locked_at: Time.now)
-      redirect_to state_file_archived_intakes_verification_error_path
+      redirect_to knock_out_path
     else
       render :edit
     end
@@ -34,6 +32,6 @@ class MailingAddressValidationController < BaseController
   end
 
   def mailing_address_validation_form_params
-    params.fetch(:state_file_archived_intakes_mailing_address_validation_form, {}).permit(:selected_address)
+    params.fetch(:mailing_address_validation_form, {}).permit(:selected_address)
   end
 end
---
+++ spec/controllers/mailing_address_validation_controller_spec.rb
@@ -0,0 +1,90 @@
+require "rails_helper"
+
+RSpec.describe MailingAddressValidationController, type: :controller do
+  let(:archived_intake) { create(:state_file_archived_intake, mailing_state: "NY") }
+  let(:email_address) { "test@example.com" }
+  let(:valid_verification_code) { "123456" }
+  let(:invalid_verification_code) { "654321" }
+
+  before do
+    allow(controller).to receive(:current_archived_intake).and_return(archived_intake)
+    allow(I18n).to receive(:locale).and_return(:en)
+    session[:code_verified] = true
+    session[:ssn_verified] = true
+  end
+
+  describe "GET #edit" do
+    it_behaves_like 'archived intake locked', action: :edit, method: :get
+
+    context "when the request is locked" do
+      before do
+        allow(archived_intake).to receive(:access_locked?).and_return(true)
+      end
+    end
+
+    context "when the request is not locked" do
+      before do
+        allow(archived_intake).to receive(:access_locked?).and_return(false)
+      end
+
+      it "renders the edit template with a new MailingAddressValidationForm" do
+        get :edit
+
+        expect(assigns(:form)).to be_a(MailingAddressValidationForm)
+        expect(response).to render_template(:edit)
+      end
+    end
+
+    it "redirects to root if code verification was not completed" do
+      session[:code_verified] = nil
+      session[:ssn_verified] = true
+      get :edit
+
+      expect(response).to redirect_to(root_path)
+    end
+
+    it "redirects to root if ssn verification was not completed" do
+      session[:code_verified] = true
+      session[:ssn_verified] = nil
+      get :edit
+
+      expect(response).to redirect_to(root_path)
+    end
+  end
+
+  describe "PATCH #update" do
+    context "with a valid chosen address" do
+      it "redirects to root path" do
+        post :update, params: {
+          mailing_address_validation_form: { selected_address: archived_intake.full_address, addresses: archived_intake.address_challenge_set}
+        }
+        expect(assigns(:form)).to be_valid
+        expect(session[:mailing_verified]).to eq(true)
+
+        # expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "with an invalid chosen address" do
+      it "creates an access log, locks the request, and redirects to the verification error path" do
+        post :update, params: {
+          mailing_address_validation_form: { selected_address: archived_intake.fake_address_1, addresses: archived_intake.address_challenge_set}
+        }
+        expect(assigns(:form)).not_to be_valid
+
+        expect(session[:mailing_verified]).to eq(nil)
+        expect(archived_intake.permanently_locked_at).to be_present
+        expect(response).to redirect_to(knock_out_path)
+      end
+    end
+
+    context "without a chosen address" do
+      it "creates an access log and re-renders the edit template" do
+        post :update, params: {}
+        expect(assigns(:form)).not_to be_valid
+
+        expect(response).to render_template(:edit)
+      end
+    end
+  end
+end
