c6493f77ecab5ca3f79204acd90b3b02a0488622
Drew Proebstel <dproebstel@codeforamerica.org>
2025-08-04 15:04:40 -0700

    add mailgun to prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,12 @@ Rails.application.configure do
   #   port: 587,
   #   authentication: :plain
   # }
+  #
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+    api_key: ENV["MAILGUN_API_KEY"],
+    domain: ENV["MAILGUN_DOMAIN"]
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
