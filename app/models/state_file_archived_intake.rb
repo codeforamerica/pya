6752a156f77621d2442d9e23d6da5a01995d309b
Drew Proebstel <dproebstel@codeforamerica.org>
2025-08-19 12:56:08 -0700

    add a TODO about unignoring a file in the linter

--- app/models/state_file_archived_intake.rb
+++ app/models/state_file_archived_intake.rb
@@ -92,6 +92,7 @@ class StateFileArchivedIntake < ApplicationRecord
   end
 
   # TODO: https://codeforamerica.atlassian.net/browse/FYST-2232 change this to look at prod s3 bucket
+  # TODO: remove this file as ignored from standard.yml
   def select_bucket
     case Rails.env
     when "development"
