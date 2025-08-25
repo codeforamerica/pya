2b559d895f0297b7576968f54cc36e9f26a6cad5
Drew Proebstel <dproebstel@codeforamerica.org>
2025-08-25 14:40:49 -0700

    define propagator as xray

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -5,3 +5,11 @@ OpenTelemetry::SDK.configure do |c|
   c.service_name = "pya"
   c.use_all
 end
+
+begin
+  require "opentelemetry/propagator/xray"
+  OpenTelemetry.propagation = OpenTelemetry::Context::Propagation.build do |b|
+    b.text_map_propagator = OpenTelemetry::Propagator::XRay::TextMapPropagator.new
+  end
+rescue LoadError
+end
