90a972734e5b3442d24ab8c4ea5eddd13821d83e
Drew Proebstel <d.proebstel@gmail.com>
2025-08-27 17:03:11 -0700

    Fyst 2088 log client activity (#142)
    * wip
    
    * give it a shot
    
    * linter
    
    * test
    
    * test
    
    * fix log
    
    * wip
    
    * define propagator as xray
    
    * linter
    
    * try new initializer
    
    * linter
    
    * update requirements in intializers
    
    * add faraday
    
    * linter
    
    * datadog loging
    
    * linter
    
    * correct intake
    
    * config dev enviornemnt
    
    * linter
    
    * logger
    
    ---------
    
    Co-authored-by: Drew Proebstel <dproebstel@codeforamerica.org>

--- app/controllers/verification_code_controller.rb
+++ app/controllers/verification_code_controller.rb
@@ -33,13 +33,9 @@ class VerificationCodeController < BaseController
 
   def update
     @form = VerificationCodeForm.new(verification_code_form_params, contact_info: current_archived_intake.contact, contact_preference: current_archived_intake.contact_preference)
-    OpenTelemetry::Trace.current_span
+    span = OpenTelemetry::Trace.current_span
     if @form.valid?
-      Rails.logger.info(
-        event: "successful verification code ",
-        intake_id: current_archived_intake.id,
-        timestamp: Time.now.utc.iso8601
-      )
+      EventLogger.log("Successful Verification Code")
       # standard:disable Style/IdenticalConditionalBranches
       case current_archived_intake.contact_preference
       when "text"
