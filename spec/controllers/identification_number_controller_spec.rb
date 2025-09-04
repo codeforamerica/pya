7ff1b64a63192c6faf7c00da30dd7571ebcb576b
Drew Proebstel <d.proebstel@gmail.com>
2025-09-04 11:41:16 -0700

    FYST-2088-log-client-activity (#147)
    * add logging to pya
    
    * remove unused logs
    
    * linter
    
    ---------
    
    Co-authored-by: Drew Proebstel <dproebstel@codeforamerica.org>

--- spec/controllers/identification_number_controller_spec.rb
+++ spec/controllers/identification_number_controller_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe IdentificationNumberController, type: :controller do
     allow(controller).to receive(:current_archived_intake).and_return(archived_intake)
     session[:email_address] = "ohhithere@example.com"
     sign_in_archived_intake(archived_intake)
+
+    allow(EventLogger).to receive(:log)
   end
 
   describe "GET #edit" do
@@ -25,32 +27,39 @@ RSpec.describe IdentificationNumberController, type: :controller do
       expect(response).to render_template(:edit)
     end
 
-    it "redirects to root if code verification was not completed" do
+    it "redirects to root if code verification was not completed and logs unauthorized attempt" do
       session[:code_verified] = nil
+
       get :edit
 
+      expect(EventLogger).to have_received(:log).with("unauthorized ssn attempt", archived_intake.id)
       expect(response).to redirect_to(root_path)
     end
   end
 
   describe "PATCH #update" do
     it_behaves_like "an authenticated archived intake controller", :patch, :update
+
     context "with a valid ssn" do
-      it "redirects to the mailing address validation page" do
+      it "redirects to the mailing address validation page and logs success + next challenge" do
+        expect(EventLogger).to receive(:log).with("correct ssn challenge", archived_intake.id).ordered
+        expect(EventLogger).to receive(:log).with("issued mailing address challenge", archived_intake.id).ordered
+
         post :update, params: {
           identification_number_form: {ssn: intake_ssn}
         }
+
         expect(assigns(:form)).to be_valid
-
         expect(session[:ssn_verified]).to eq(true)
-
         expect(archived_intake.reload.failed_attempts).to eq(0)
-
         expect(response).to redirect_to(edit_mailing_address_validation_path)
       end
 
       it "resets failed attempts to zero even if one failed attempt has already been made" do
         archived_intake.update!(failed_attempts: 1)
+
+        expect(EventLogger).to receive(:log).with("correct ssn challenge", archived_intake.id).ordered
+        expect(EventLogger).to receive(:log).with("issued mailing address challenge", archived_intake.id).ordered
 
         post :update, params: {
           identification_number_form: {ssn: intake_ssn}
@@ -62,18 +71,20 @@ RSpec.describe IdentificationNumberController, type: :controller do
     end
 
     context "with an invalid ssn" do
-      before do
-        allow_any_instance_of(VerificationCodeForm).to receive(:valid?).and_return(false)
-      end
+      it "increments failed_attempts, logs incorrect attempt, and re-renders edit on first failed attempt" do
+        expect(EventLogger).to receive(:log).with("incorrect ssn challenge", archived_intake.id)
 
-      it "increments failed_attempts, and re-renders edit on first failed attempt" do
         post :update, params: {identification_number_form: {ssn: invalid_ssn}}
+
         expect(archived_intake.reload.failed_attempts).to eq(1)
         expect(response).to render_template(:edit)
       end
 
-      it "locks the account and redirects to root path after multiple failed attempts" do
+      it "locks the account, logs lockout begin, and redirects to knock out after multiple failed attempts" do
         archived_intake.update!(failed_attempts: 1)
+
+        expect(EventLogger).to receive(:log).with("incorrect ssn challenge", archived_intake.id).ordered
+        expect(EventLogger).to receive(:log).with("client lockout begin", archived_intake.id).ordered
 
         post :update, params: {identification_number_form: {ssn: invalid_ssn}}
 
