f7c5317ab57c9e44f3af2b8219d36a320f555462
Drew Proebstel <dproebstel@codeforamerica.org>
2025-11-14 09:43:54 -0800

    test var problem

--- app/services/ssn_hashing_service.rb
+++ app/services/ssn_hashing_service.rb
@@ -2,7 +2,7 @@ class SsnHashingService
   def self.hash(ssn)
     OpenSSL::HMAC.hexdigest(
       "SHA256",
-      ENV["SSN_HASHING_KEY"],
+      "test_hashing_key_1234567890_abcdef_TEST_ONLY",
       "ssn|#{ssn}"
     )
   end
