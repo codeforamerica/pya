329b516514a1def6ed259ad61b46cb4753c0e925
Drew Proebstel <dproebstel@codeforamerica.org>
2025-11-14 20:35:37 -0800

    allow some session varriables to persist

--- app/controllers/state_file_archived_intakes/sessions_controller.rb
+++ app/controllers/state_file_archived_intakes/sessions_controller.rb
@@ -2,7 +2,9 @@ class StateFileArchivedIntakes::SessionsController < Devise::SessionsController
 
   def destroy
     year_selected = session[:year_selected]
+    permanently_locked = session[:permanently_locked]
     super
     session[:year_selected] = year_selected if year_selected.present?
+    session[:permanently_locked] = permanently_locked if permanently_locked.present?
   end
 end
